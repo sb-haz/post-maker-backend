37ed2434d6cc221c9a8a74d30a9f19f7ae849490
sb-haz <sb_haz@icloud.com>
2022-02-18 19:47:16 +0000

    add quote render on static

--- request_api.py
+++ request_api.py
@@ -3,6 +3,6 @@ import requests
 BASE = "http://127.0.0.1:5000/"
 
 response = requests.post(
-    BASE + "tool/video", {"tweet_url": "https://twitter.com/tinaqueen_15/status/1469709115144478729?s=20&t=WmrW8R5mDRT-hm83AJRakA", "watermark": "finesstv", "email": "teamfinesstv@gmail.com"})
+    BASE + "tool/quote", {"tweet_url": "https://twitter.com/tinaqueen_15/status/1469709115144478729?s=20&t=WmrW8R5mDRT-hm83AJRakA", "watermark": "finesstv", "email": "teamfinesstv@gmail.com"})
 
 # print(response.json())
