f364bf33d96f381e00e7e1409b41ff18b45dcae1
sb-haz <sb_haz@icloud.com>
2022-02-19 18:47:51 +0000

    restructure dir

--- request_api.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-BASE = "http://127.0.0.1:5000/"
-
-response = requests.post(
-    BASE + "tool/quote", {"tweet_url": "https://twitter.com/tinaqueen_15/status/1469709115144478729?s=20&t=WmrW8R5mDRT-hm83AJRakA", "watermark": "finesstv", "email": "teamfinesstv@gmail.com"})
-
-# print(response.json())
